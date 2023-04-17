8f64b8e3dc263f73b6feef8f399f81cf1c13c069
Shivam seth <shivamseth221104@gmail.com>
2023-04-17 22:47:59 +0530

    palindrome-checker-js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("btn").addEventListener("click",function(){
+    let txt = document.getElementById("input-text").value;
+    checkPalindrome(txt);
+});
+function checkPalindrome(txt) {
+    let txt_new = txt.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let len = txt_new.length;
+    let halfLen =Math.floor(len/2);
+    let result = document.getElementById("result")  ;
+    let i ;
+    for(i =0 ; i< halfLen ; i++){
+        if(txt_new[i] !== txt_new[len-1-i]){
+            result.textContent = "Not a palindrome";
+            return;
+        }
+        result.textContent ="yep!! palindrome"
+    }  
+}
